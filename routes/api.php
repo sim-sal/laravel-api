f8e530ef760bd335510ddb1b94259a4b19d58f84
sim-sal <simone.saladino93@gmail.com>
2023-08-22 20:00:47 +0200

    add Route in api.php to convert all data in json

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Api\ProjectController;
+
+Route::prefix('v1')->group(function () {
+
+    Route::get('/projects', [ProjectController::class, 'projectsIndex']);
+});
